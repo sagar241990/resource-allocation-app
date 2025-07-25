5f0f7c1208506307350866c0a23a452981ba1e5b
Sagar <sagar241990@gmail.com>
2025-07-25 14:57:13 +0530

    basic proj

---
+++ src/main/java/com/sag/res_allocation_app/resource_allocation_app/ResourceAllocationAppApplication.java
@@ -0,0 +1,13 @@
+package com.sag.res_allocation_app.resource_allocation_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceAllocationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceAllocationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sag/res_allocation_app/resource_allocation_app/ResourceAllocationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sag.res_allocation_app.resource_allocation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceAllocationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
